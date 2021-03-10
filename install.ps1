4bf01bba01a1a0f433aed8c2b62a9a18ba6f3dcc
Moncef Gaha <moncefplastin07@gmail.com>
2021-03-10 08:20:01 +0100

    add the cli path in to the environment variable

--- install.ps1
+++ install.ps1
@@ -1,4 +1,22 @@
 $CliUri = "https://github.com/moncefplastin07/ytb-dl/raw/main/ytb-dl.exe"
-Invoke-WebRequest $CliUri -OutFile "C:\clis\ytb-dl.exe" -UseBasicParsing
+$BinDir = "$Home\.ytb-dl\bin"
+$CliPath = "$BinDir\ytb-dl.exe"
 
-Write-Output "YTB CLI was installed successfully"
+if (!(Test-Path $BinDir)) {
+    New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+if (!(Test-Path $BinDir)) {
+    New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $CliUri -OutFile $CliPath -UseBasicParsing
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "YTB CLI was installed successfully $BinDir"
